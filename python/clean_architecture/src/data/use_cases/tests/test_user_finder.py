f24677d19c162f5cb6e2e456332e6c042aa6f371
cristian <cristiantcjti@gmail.com>
2024-06-16 19:20:04 -0300

    Refactor user_finder and tests.

--- python/clean_architecture/src/data/use_cases/tests/test_user_finder.py
+++ python/clean_architecture/src/data/use_cases/tests/test_user_finder.py
@@ -1,16 +1,49 @@
+import pytest
 from src.infra.db.tests.users_repository import UsersRepositorySpy
 from src.data.use_cases.user_finder import UserFinder
 
 
-def test_find():
-    first_name = "NameTest"
+class TestUserFinder:
+    def test_find(self):
+        first_name = "NameTest"
 
-    repo = UsersRepositorySpy()
-    user_finder = UserFinder(repo)
+        repo = UsersRepositorySpy()
+        user_finder = UserFinder(repo)
 
-    response = user_finder.find(first_name)
+        response = user_finder.find(first_name)
 
-    assert repo.select_user_attributes["first_name"] == first_name
-    assert response["type"] == "users"
-    assert response["count"] == len(response["attributes"])
-    assert response["attributes"] != []
+        assert repo.select_user_attributes["first_name"] == first_name
+        assert response["type"] == "users"
+        assert response["count"] == len(response["attributes"])
+        assert response["attributes"] != []
+
+    def test_find_error_in_valid_name(self):
+        first_name = "NameTest123"
+        repo = UsersRepositorySpy()
+        user_finder = UserFinder(repo)
+
+        with pytest.raises(Exception) as error:
+            user_finder.find(first_name)
+
+        assert str(error.value) == "Invalid name for searching"
+
+    def test_find_error_in_name_too_long(self):
+        first_name = "TestNameTooLongInMethodFind"
+        repo = UsersRepositorySpy()
+        user_finder = UserFinder(repo)
+
+        with pytest.raises(Exception) as error:
+            user_finder.find(first_name)
+
+        assert str(error.value) == "Name too long"
+
+    def test_find_error_user_not_found(self, mocker):
+        first_name = "TestName"
+        UsersRepositorySpy.select_user = mocker.Mock(return_value=[])
+        repo = UsersRepositorySpy()
+        user_finder = UserFinder(repo)
+
+        with pytest.raises(Exception) as error:
+            user_finder.find(first_name)
+
+        assert str(error.value) == "User not found"
--- python/clean_architecture/src/data/use_cases/user_finder.py
+++ python/clean_architecture/src/data/use_cases/user_finder.py
@@ -1,5 +1,6 @@
-from typing import Dict
+from typing import Dict, Optional
 from src.data.interfaces.users_repository import UsersRepositoryInterface
+from src.domain.models.users import Users
 from src.domain.use_cases.user_finder import UserFinder as UserFinderInterface
 
 
@@ -8,14 +9,25 @@ class UserFinder(UserFinderInterface):
         self._users_repository = users_repository
 
     def find(self, first_name: str) -> Dict:
+        self._validate_name(first_name)
+        users = self._search_user(first_name)
+        return self._format_response(users)
+
+    @classmethod
+    def _validate_name(cls, first_name: str):
         if not first_name.isalpha():
             raise Exception("Invalid name for searching")
 
         if len(first_name) > 18:
             raise Exception("Name too long")
 
+    def _search_user(self, first_name: str) -> list[Users]:
         users = self._users_repository.select_user(first_name)
         if users == []:
             raise Exception("User not found")
+        return users
 
+    @classmethod
+    def _format_response(cls, users: list[Users]) -> dict:
+        users = [{"first_name": user.first_name, "age": user.age} for user in users]
         return {"type": "users", "count": len(users), "attributes": users}
